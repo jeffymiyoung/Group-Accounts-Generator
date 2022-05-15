9d1777103f70ce04c3ed15469800f4aae8105cac
Jeffrey Young <jeffreymiyoung@aol.com>
2022-05-14 21:38:17 -0400

    added Employee Testing

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,36 +1,36 @@
 // testing code for Employee role
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee.js');
 
 // Employee Object with Name, ID, and Email then get the get___() method of capture
 test('Gets Employee Object', () => {
-    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
+    const employee = new Employee('Jeffrey', 17, 'jeffreymiyoung@aol.com');
     
-    expect(employee.name).toBe(expect.any(String));
-    expect(employee.id).toBe(expect.any(Number));
-    expect(employee.email).toBe(expect.any(String));
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
 
 test('Gets Employee name using getName()', () => {
-    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
+    const employee = new Employee('Jeffrey', 17, 'jeffreymiyoung@aol.com');
 
-    expect(employee.getName()).toBe(expect.any(String));
+    expect(employee.getName()).toEqual(expect.any(String));
 });
 
 test('Get Employee getId()', () => {
-    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
+    const employee = new Employee('Jeffrey', 17, 'jeffreymiyoung@aol.com');
 
-    expect(employee.getId()).toBe(expect.any(Number));
+    expect(employee.getId()).toEqual(expect.any(Number));
 });
 
 test('Get Employee getEmail()', () => {
-    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
+    const employee = new Employee('Jeffrey', 17, 'jeffreymiyoung@aol.com');
 
-    expect(employe.getEmail()).toBe(expect.any(String));
+    expect(employee.getEmail()).toEqual(expect.any(String));
 });
 
 // Override Company Role toEqual Employee
 test('Get Employee getRole()', () => {
-    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
+    const employee = new Employee('Jeffrey', 17, 'jeffreymiyoung@aol.com');
 
-    expect(employee.getRole()).toBe("Employee");
+    expect(employee.getRole()).toEqual("Employee");
 });
