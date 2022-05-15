271be7fe03d391bfc401832e7ff496f3f7d058f0
Jeffrey Young <jeffreymiyoung@aol.com>
2022-05-14 21:19:31 -0400

    updated files

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,37 +1,36 @@
 // testing code for Employee role
 const Employee = require('../lib/Employee');
 
-test('Gets Employee name', () => {
-    const name = "David";
-    const Employee = new Employee(name);
-    expect(Employee.name).toBe(name);  
+// Employee Object with Name, ID, and Email then get the get___() method of capture
+test('Gets Employee Object', () => {
+    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
     
-})
+    expect(employee.name).toBe(expect.any(String));
+    expect(employee.id).toBe(expect.any(Number));
+    expect(employee.email).toBe(expect.any(String));
+});
 
 test('Gets Employee name using getName()', () => {
-    const testName = "David";
-    const Employee = new Employee(testName);
-    expect(Employee.getName()).toBe(testName);  
-})
+    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
 
-test('Get Employee ID', () => {
-    const Employee = new Employee()
-
-})
+    expect(employee.getName()).toBe(expect.any(String));
+});
 
 test('Get Employee getId()', () => {
+    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
 
-})
-
-test('Get Employee Email', () => {
-
-})
+    expect(employee.getId()).toBe(expect.any(Number));
+});
 
 test('Get Employee getEmail()', () => {
+    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
 
-})
+    expect(employe.getEmail()).toBe(expect.any(String));
+});
 
+// Override Company Role toEqual Employee
 test('Get Employee getRole()', () => {
-    // overridden as Employee
-    
-})
+    const employee = new Employee('Jeffrey', 017, 'jeffreymiyoung@aol.com');
+
+    expect(employee.getRole()).toBe("Employee");
+});
