d6c4841efd897f686461f7eeac78158204a92f8a
Jeffrey Young <jeffreymiyoung@aol.com>
2022-05-14 22:11:26 -0400

    Added Intern testing

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,3 +2,21 @@
 const Intern = require('../lib/Intern.js');
 
 // Intern Object with Name, Id, Email, and School - getSchool()
+test('Get Intern Object', () => {
+    const intern = new Intern('Jeffrey', 17, 'jeffreymiyoung@aol.com', 'Felician University')
+
+    expect(intern.school).toEqual(expect.any(String));
+})
+
+test('Check getSchool()', () => {
+    const intern = new Intern('Jeffrey', 17, 'jeffreymiyoung@aol.com', 'Felician University')
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+// Override getRole() to equal Intern
+test('Get Employee getRole()', () => {
+    const intern = new Intern('Jeffrey', 17, 'jeffreymiyoung@aol.com', 'Felician University')
+
+    expect(intern.getRole()).toEqual("Intern");
+});
