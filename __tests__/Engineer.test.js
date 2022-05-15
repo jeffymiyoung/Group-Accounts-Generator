e5d79c1819e72f86f804192b0eadd9ced37942c4
Jeffrey Young <jeffreymiyoung@aol.com>
2022-05-14 22:02:06 -0400

    added Engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+// testing code for Engineer role
+const Engineer = require('../lib/Engineer.js');
+
+// Engineer Object with Name, Id, Email, and GitHub Username - with getGithub()
+test('Get Engineer Object', () => {
+    const engineer = new Engineer('Jeffrey', 17, 'jeffreymiyoung@aol.com', 'jeffymiyoung'); 
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test('checks getGitHub()', () => {
+    const engineer = new Engineer('Jeffrey', 17, 'jeffreymiyoung@aol.com', 'jeffymiyoung');
+
+    expect(engineer.getGitHub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// Override Company Role toEqual Engineer
+test('Get Employee getRole()', () => {
+    const engineer = new Engineer('Jeffrey', 17, 'jeffreymiyoung@aol.com', 'jeffymiyoung');
+
+    expect(engineer.getRole()).toEqual('Engineer');
+});
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,4 @@
+// testing code for Intern role
+const Intern = require('../lib/Intern.js');
+
+// Intern Object with Name, Id, Email, and School - getSchool()
